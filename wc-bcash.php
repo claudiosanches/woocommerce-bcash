00cd940332ef4c82ace1f9bc8c5d00cb39bca6f9
claudiosmweb <contato@claudiosmweb.com>
2013-12-09 23:40:35 -0200

    fixed some standards

--- wc-bcash.php
+++ wc-bcash.php
@@ -15,7 +15,7 @@
  * WooCommerce fallback notice.
  */
 function wcbcash_woocommerce_fallback_notice() {
-    echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Bcash Gateway depends on the last version of %s to work!', 'wcbcash' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
+	echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Bcash Gateway depends on the last version of %s to work!', 'wcbcash' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
 }
 
 /**
@@ -23,34 +23,34 @@ function wcbcash_woocommerce_fallback_notice() {
  */
 function wcbcash_gateway_load() {
 
-    if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
-        add_action( 'admin_notices', 'wcbcash_woocommerce_fallback_notice' );
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		add_action( 'admin_notices', 'wcbcash_woocommerce_fallback_notice' );
 
-        return;
-    }
+		return;
+	}
 
-    /**
-     * Load textdomain.
-     */
-    load_plugin_textdomain( 'wcbcash', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	/**
+	 * Load textdomain.
+	 */
+	load_plugin_textdomain( 'wcbcash', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-    /**
-     * Add the gateway to WooCommerce.
-     *
-     * @param  array $methods WooCommerce payment methods.
-     *
-     * @return array          Payment methods with Bcash.
-     */
-    add_filter( 'woocommerce_payment_gateways', 'wcbcash_add_gateway' );
+	/**
+	 * Add the gateway to WooCommerce.
+	 *
+	 * @param  array $methods WooCommerce payment methods.
+	 *
+	 * @return array          Payment methods with Bcash.
+	 */
+	add_filter( 'woocommerce_payment_gateways', 'wcbcash_add_gateway' );
 
-    function wcbcash_add_gateway( $methods ) {
-        $methods[] = 'WC_BCash_Gateway';
+	function wcbcash_add_gateway( $methods ) {
+		$methods[] = 'WC_BCash_Gateway';
 
-        return $methods;
-    }
+		return $methods;
+	}
 
-    // Include the WC_BCash_Gateway class.
-    require_once plugin_dir_path( __FILE__ ) . 'class-wc-bcash-gateway.php';
+	// Include the WC_BCash_Gateway class.
+	require_once plugin_dir_path( __FILE__ ) . 'class-wc-bcash-gateway.php';
 }
 
 add_action( 'plugins_loaded', 'wcbcash_gateway_load', 0 );
@@ -61,13 +61,13 @@ add_action( 'plugins_loaded', 'wcbcash_gateway_load', 0 );
  * @return void
  */
 function wcbcash_legacy_ipn() {
-    if ( isset( $_POST['id_pedido'] ) && ! isset( $_GET['wc-api'] ) ) {
-        global $woocommerce;
+	if ( isset( $_POST['id_pedido'] ) && ! isset( $_GET['wc-api'] ) ) {
+		global $woocommerce;
 
-        $woocommerce->payment_gateways();
+		$woocommerce->payment_gateways();
 
-        do_action( 'woocommerce_api_wc_bcash_gateway' );
-    }
+		do_action( 'woocommerce_api_wc_bcash_gateway' );
+	}
 }
 
 add_action( 'init', 'wcbcash_legacy_ipn' );
@@ -81,11 +81,11 @@ add_action( 'init', 'wcbcash_legacy_ipn' );
  */
 function wcbcash_hides_when_is_outside_brazil( $available_gateways ) {
 
-    // Remove standard shipping option.
-    if ( isset( $_REQUEST['country'] ) && $_REQUEST['country'] != 'BR' )
-        unset( $available_gateways['bcash'] );
+	// Remove standard shipping option.
+	if ( isset( $_REQUEST['country'] ) && $_REQUEST['country'] != 'BR' )
+		unset( $available_gateways['bcash'] );
 
-    return $available_gateways;
+	return $available_gateways;
 }
 
 add_filter( 'woocommerce_available_payment_gateways', 'wcbcash_hides_when_is_outside_brazil' );
@@ -99,15 +99,15 @@ add_filter( 'woocommerce_available_payment_gateways', 'wcbcash_hides_when_is_out
  */
 function wcbcash_action_links( $links ) {
 
-    $settings = array(
-        'settings' => sprintf(
-            '<a href="%s">%s</a>',
-            admin_url( 'admin.php?page=woocommerce_settings&tab=payment_gateways&section=WC_BCash_Gateway' ),
-            __( 'Settings', 'wcbcash' )
-        )
-    );
+	$settings = array(
+		'settings' => sprintf(
+			'<a href="%s">%s</a>',
+			admin_url( 'admin.php?page=woocommerce_settings&tab=payment_gateways&section=WC_BCash_Gateway' ),
+			__( 'Settings', 'wcbcash' )
+		)
+	);
 
-    return array_merge( $settings, $links );
+	return array_merge( $settings, $links );
 }
 
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wcbcash_action_links' );
