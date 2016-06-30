c5e37d2fb88b8137782e5a48f5d5b88d1fc699f4
Claudio Sanches <contato@claudiosmweb.com>
2016-06-30 19:29:25 -0300

    Updated grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,45 @@ module.exports = function( grunt ) {
 			]
 		},
 
+		// Make .pot files
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-plugin'
+				}
+			}
+		},
+
+		// Check text domain
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
+
 		// Rsync commands used to take the files to svn repository
 		rsync: {
 			options: {
@@ -89,6 +128,8 @@ module.exports = function( grunt ) {
 	// Load tasks
 	grunt.loadNpmTasks( 'grunt-rsync' );
 	grunt.loadNpmTasks( 'grunt-shell' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 
 	// Deploy task
 	grunt.registerTask( 'deploy', [
